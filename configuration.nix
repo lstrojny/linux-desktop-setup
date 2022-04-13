2b9dc1cb9ef3f87536316354f8a35728bdda42a0
Lars Strojny <lars@strojny.net>
2022-04-13 09:24:20 +0200

    Use bluez with all plugins

--- configuration.nix
+++ configuration.nix
@@ -96,6 +96,7 @@
   # Bluetooth
   services.blueman.enable = true;
   hardware.bluetooth.enable = true;
+  hardware.bluetooth.package = pkgs.bluezFull;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
