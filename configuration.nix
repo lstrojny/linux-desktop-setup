965819a4db66a2538277983091a39c1515cde9d8
Lars Strojny <lars@strojny.net>
2022-04-16 18:32:11 +0200

    sudo without password

--- configuration.nix
+++ configuration.nix
@@ -111,6 +111,7 @@
     hashedPassword =
       "$6$3wh8GiYYOgFm321d$8CXIw54QA8FrrbbQB/XtIMUQq11CFVPzvMwg1vdDgW7bTFd92i6pihw9JQR2nqQfinCpK0dVgdRp7XuazWvaL.";
   };
+  security.sudo.wheelNeedsPassword = false;
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
