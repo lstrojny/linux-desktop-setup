45e7f7aaaa5cce26cbb19e2bc60a09530303371b
Lars Strojny <lars@strojny.net>
2022-04-16 22:52:27 +0200

    Configure vim

--- home.nix
+++ home.nix
@@ -1,3 +1,4 @@
+{ pkgs, ... }:
 with (import ./settings.nix); {
   programs.git = {
     enable = true;
@@ -19,6 +20,47 @@ with (import ./settings.nix); {
     shellAliases = { g = "git"; };
     bashrcExtra = ''
       PATH=$PATH:./vendor/bin:./node-modules/bin
+    '';
+  };
+  programs.vim = {
+    enable = true;
+    plugins = [ pkgs.vimPlugins.vim-polyglot pkgs.vimPlugins.vim-scala pkgs.vimPlugins.vim-colors-solarized ];
+    settings = {
+      background = "dark";
+      expandtab = true;
+      tabstop = 4;
+      history = 10000;
+    };
+    extraConfig = ''
+      " Use system clipboard
+      set clipboard=unnamedplus
+
+      " Backspace deletes anything
+      set backspace=2
+
+      " Enable line numbers
+      set nu!
+
+      " It's 2020 something
+      set textwidth=120
+
+      " Highlight trailing whitespace
+      highlight WhitespaceEOL ctermbg=red guibg=red
+      match WhitespaceEOL /\s\+$/
+
+      " Fix my favorite typos
+      cnoreabbrev X x
+      cnoreabbrev W w
+      cnoreabbrev Q q
+
+      "Make command tab completion usable
+      set wildmenu
+      set wildmode=list:longest
+
+      set title
+
+      " Solarized
+      colorscheme solarized
     '';
   };
 }
