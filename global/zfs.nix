200a16bec0bdc5351ef4fe635c0d3905a8b75314
Lars Strojny <lars@strojny.net>
2022-04-18 11:09:48 +0200

    Configure grub

--- global/zfs.nix
+++ global/zfs.nix
@@ -1,22 +1,33 @@
 { config, pkgs, ... }:
 
 {
-  boot.loader.grub.enable = true;
-  boot.loader.grub.efiSupport = true;
-  boot.loader.grub.device = "nodev";
-  boot.loader.grub.mirroredBoots = [{
-    devices = [ "/dev/disk/by-label/BOOT-FB" ];
-    path = "/boot-fallback";
-  }];
-  boot.supportedFilesystems = [ "zfs" ];
-  boot.zfs.devNodes = "/dev/disk/by-label/zroot";
-  boot.zfs.enableUnstable = true;
-  boot.kernelParams = [ "nohibernate" ];
-  services.zfs.autoScrub.enable = true;
-  services.zfs.trim.enable = true;
-  services.zfs.autoSnapshot = {
-    enable = true;
-    frequent = 8;
-    monthly = 6;
+  boot = {
+    supportedFilesystems = [ "zfs" ];
+    kernelParams = [ "nohibernate" ];
+    loader.grub = {
+      enable = true;
+      efiSupport = true;
+      device = "nodev";
+      mirroredBoots = [{
+        devices = [ "/dev/disk/by-label/BOOT-FB" ];
+        path = "/boot-fallback";
+      }];
+
+      gfxmodeEfi = "2560x1440";
+    };
+
+    zfs = {
+      devNodes = "/dev/disk/by-label/zroot";
+      enableUnstable = true;
+    };
+  };
+  services.zfs = {
+    autoScrub.enable = true;
+    trim.enable = true;
+    autoSnapshot = {
+      enable = true;
+      frequent = 8;
+      monthly = 6;
+    };
   };
 }
