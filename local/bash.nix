a09dd4b210902ed2df6ff9dc4007a32b1cfa934a
Lars Strojny <lars@strojny.net>
2022-04-23 15:18:21 +0200

    Configure lesspipe

--- local/bash.nix
+++ local/bash.nix
@@ -36,7 +36,13 @@
       # Mouse support in less
       LESS="--mouse"
       # Syntax highlighting for less
-      LESSOPEN='|pygmentize -g %s'
+      eval `lesspipe`
+      LESS_TERMCAP_md=$'\e[01;35m' \
+      LESS_TERMCAP_me=$'\e[0m' \
+      LESS_TERMCAP_us=$'\e[01;32m' \
+      LESS_TERMCAP_ue=$'\e[0m' \
+      LESS_TERMCAP_so=$'\e[45;30m' \
+      LESS_TERMCAP_se=$'\e[0m' \
     '';
   };
   programs.powerline-go = {
